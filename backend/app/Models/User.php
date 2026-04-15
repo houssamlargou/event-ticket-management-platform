e0d263b3b215f5603b81245856ac0405392f6a7a
Houssam <hossamlargo5@gmail.com>
2026-04-15 06:59:15 +0100

    ETM-10 implement register endpoint #comment Added register endpoint using Form Request and AuthService, validated input, created user, and returned Sanctum token #Done

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,13 +9,24 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $hidden = [
+    'password',
+    'remember_token',
+    ];
+
+    protected $fillable = [
+    'name',
+    'email',
+    'password',
+    ];
 
     /**
      * Get the attributes that should be cast.
